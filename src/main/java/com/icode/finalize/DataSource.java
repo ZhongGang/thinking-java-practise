08d6e24be74cbcafa3d8ead947a48a0576dabd8e
ZhongGang <120859274@qq.com>
2013-07-07 18:57:14 +0800

    finalize the object

---
+++ src/main/java/com/icode/finalize/DataSource.java
@@ -0,0 +1,28 @@
+package com.icode.finalize;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ZhongGang
+ * Date: 13-7-7
+ * Time: 下午6:49
+ */
+public class DataSource {
+    private String url;
+    private Class<?> driverClassName;
+    private String username;
+    private String password;
+
+
+    public void finalize() {
+        System.out.println("Free the memory~");
+    }
+
+    public static void main(String[] args) {
+        new DataSource();
+        new DataSource();
+        new DataSource();
+
+        System.gc();
+    }
+}
+
