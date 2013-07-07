e18df7d168e461ad5a832f957533f61ddb2bbab5
ZhongGang <120859274@qq.com>
2013-07-07 19:57:44 +0800

    initialize

---
+++ src/main/java/com/icode/initialize/Initialize.java
@@ -0,0 +1,29 @@
+package com.icode.initialize;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ZhongGang
+ * Date: 13-7-7
+ * Time: 下午7:16
+ */
+public class Initialize {
+    int a;
+    short b = 2;
+    float c = 3.1f;
+    double d;
+    boolean e;
+    byte f;
+    char g;
+    long h;
+
+    public void print() {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
---
+++ src/main/java/com/icode/initialize/Main.java
@@ -0,0 +1,15 @@
+package com.icode.initialize;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ZhongGang
+ * Date: 13-7-7
+ * Time: 下午7:08
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Initialize initialize = new Initialize();
+        initialize.print();
+    }
+}
