45b91d0a321c11e3ee5981ca879931ae357d7882
ZhongGang <120859274@qq.com>
2013-07-08 16:35:14 +0800

    array

--- src/main/java/com/icode/array/Main.java
+++ src/main/java/com/icode/array/Main.java
@@ -10,5 +10,14 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayFill.print();
+
+
+        int[] arrays = new int[5];
+        arrays[0] = 1;
+        arrays[1] = 2;
+        arrays[2] = 3;
+        arrays[3] = 4;
+        arrays[4] = 5;
+        arrays[5] = 6;
     }
 }
