471c33fb35ad5a40ba5c30a63d5203fc18a8911c
ZhongGang <120859274@qq.com>
2013-07-12 13:40:46 +0800

    nest interfaces

---
+++ src/main/java/com/icode/nest/Writable.java
@@ -0,0 +1,17 @@
+package com.icode.nest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ZhongGang
+ * Date: 13-7-12
+ * Time: 下午1:33
+ */
+public interface Writable {
+
+    void write();
+
+    public interface Readable {
+
+        void read();
+    }
+}
---
+++ src/main/java/com/icode/nest/WritableImpl.java
@@ -0,0 +1,15 @@
+package com.icode.nest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ZhongGang
+ * Date: 13-7-12
+ * Time: 下午1:35
+ */
+public class WritableImpl implements Writable {
+
+    @Override
+    public void write() {
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+}
